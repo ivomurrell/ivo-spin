a6fc73b17c65bcf69d072cef74181e0c55573aad
Ivo Murrell <ivoivo@hey.com>
2024-06-26 14:53:31 +0100

    use an index buffer to render a rectangle

--- src/vulkan.zig
+++ src/vulkan.zig
@@ -27,6 +27,7 @@ graphics_pipeline: c.VkPipeline,
 framebuffers: []c.VkFramebuffer,
 command_pool: c.VkCommandPool,
 vertex_buffer: VertexBuffer,
+index_buffer: IndexBuffer,
 command_buffers: [frames_in_flight]c.VkCommandBuffer,
 image_available_semaphores: [frames_in_flight]c.VkSemaphore,
 render_finished_semaphores: [frames_in_flight]c.VkSemaphore,
@@ -122,6 +123,13 @@ pub fn init(allocator: Allocator, sdl: *const SDL) !Self {
         graphics_queue,
     );
     errdefer vertex_buffer.deinit();
+    const index_buffer = try initIndexBuffer(
+        device,
+        physical_device,
+        command_pool,
+        graphics_queue,
+    );
+    errdefer index_buffer.deinit();
 
     const command_buffers = try initCommandBuffers(device, command_pool);
 
@@ -156,6 +164,7 @@ pub fn init(allocator: Allocator, sdl: *const SDL) !Self {
         .framebuffers = framebuffers,
         .command_pool = command_pool,
         .vertex_buffer = vertex_buffer,
+        .index_buffer = index_buffer,
         .command_buffers = command_buffers,
         .image_available_semaphores = synchronisation_objects.image_available_semaphores,
         .render_finished_semaphores = synchronisation_objects.render_finished_semaphores,
@@ -178,6 +187,7 @@ pub fn deinit(self: Self) void {
     for (self.in_flight_fences) |in_flight_fence| {
         c.vkDestroyFence(self.device, in_flight_fence, null);
     }
+    self.index_buffer.deinit();
     self.vertex_buffer.deinit();
     c.vkDestroyCommandPool(self.device, self.command_pool, null);
     for (self.framebuffers) |framebuffer| {
@@ -719,17 +729,31 @@ fn initVertexBuffer(
     graphics_queue: c.VkQueue,
 ) !VertexBuffer {
     const vertices = [_]Vertex{ .{
-        .position = [_]f32{ 0.0, -0.5 },
+        .position = [_]f32{ -0.5, -0.5 },
         .colour = [_]f32{ 1.0, 0.0, 0.0 },
     }, .{
-        .position = [_]f32{ 0.5, 0.5 },
+        .position = [_]f32{ 0.5, -0.5 },
         .colour = [_]f32{ 0.0, 1.0, 0.0 },
     }, .{
-        .position = [_]f32{ -0.5, 0.5 },
+        .position = [_]f32{ 0.5, 0.5 },
         .colour = [_]f32{ 0.0, 0.0, 1.0 },
+    }, .{
+        .position = [_]f32{ -0.5, 0.5 },
+        .colour = [_]f32{ 1.0, 1.0, 1.0 },
     } };
 
     return VertexBuffer.init(&vertices, device, physical_device, command_pool, graphics_queue);
+}
+
+fn initIndexBuffer(
+    device: c.VkDevice,
+    physical_device: c.VkPhysicalDevice,
+    command_pool: c.VkCommandPool,
+    graphics_queue: c.VkQueue,
+) !IndexBuffer {
+    const indices = [_]u16{ 0, 1, 2, 2, 3, 0 };
+
+    return IndexBuffer.init(&indices, device, physical_device, command_pool, graphics_queue);
 }
 
 fn initCommandBuffers(
@@ -851,8 +875,14 @@ fn recordCommandBuffer(self: *Self, framebuffer: c.VkFramebuffer) !void {
         &self.vertex_buffer.handle.handle,
         &@as(u64, 0),
     );
+    c.vkCmdBindIndexBuffer(
+        command_buffer,
+        self.index_buffer.handle.handle,
+        0,
+        c.VK_INDEX_TYPE_UINT16,
+    );
 
-    c.vkCmdDraw(command_buffer, @intCast(self.vertex_buffer.vertices.len), 1, 0, 0);
+    c.vkCmdDrawIndexed(command_buffer, @intCast(self.index_buffer.data.len), 1, 0, 0, 0);
 
     c.vkCmdEndRenderPass(command_buffer);
     try wrapVulkanResult(c.vkEndCommandBuffer(command_buffer), "failed to record command buffer");
@@ -1038,105 +1068,111 @@ pub const Buffer = struct {
     }
 };
 
-pub const VertexBuffer = struct {
-    vertices: []const Vertex,
-    handle: Buffer,
+fn DataBuffer(comptime T: type, buffer_type: u32) type {
+    return struct {
+        const SelfBuffer = @This();
+        data: []const T,
+        handle: Buffer,
 
-    pub fn init(
-        vertices: []const Vertex,
-        device: c.VkDevice,
-        physical_device: c.VkPhysicalDevice,
-        command_pool: c.VkCommandPool,
-        graphics_queue: c.VkQueue,
-    ) !VertexBuffer {
-        const buffer_size = @sizeOf(Vertex) * vertices.len;
+        pub fn init(
+            data: []const T,
+            device: c.VkDevice,
+            physical_device: c.VkPhysicalDevice,
+            command_pool: c.VkCommandPool,
+            graphics_queue: c.VkQueue,
+        ) !SelfBuffer {
+            const buffer_size = @sizeOf(T) * data.len;
 
-        const staging_buffer = try Buffer.init(
-            device,
-            physical_device,
-            buffer_size,
-            c.VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-            c.VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | c.VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-        );
-        defer staging_buffer.deinit();
+            const staging_buffer = try Buffer.init(
+                device,
+                physical_device,
+                buffer_size,
+                c.VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                c.VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | c.VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            );
+            defer staging_buffer.deinit();
 
-        var mapped_data: [*]Vertex = undefined;
-        try wrapVulkanResult(
-            c.vkMapMemory(device, staging_buffer.memory, 0, buffer_size, 0, @ptrCast(&mapped_data)),
-            "failed to map vertex buffer memory",
-        );
-        @memcpy(mapped_data, vertices);
-        c.vkUnmapMemory(device, staging_buffer.memory);
+            var mapped_data: [*]T = undefined;
+            try wrapVulkanResult(
+                c.vkMapMemory(device, staging_buffer.memory, 0, buffer_size, 0, @ptrCast(&mapped_data)),
+                "failed to map vertex buffer memory",
+            );
+            @memcpy(mapped_data, data);
+            c.vkUnmapMemory(device, staging_buffer.memory);
 
-        const vertex_buffer = try Buffer.init(
-            device,
-            physical_device,
-            buffer_size,
-            c.VK_IMAGE_USAGE_TRANSFER_DST_BIT | c.VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
-            c.VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-        );
-        errdefer vertex_buffer.deinit();
+            const data_buffer = try Buffer.init(
+                device,
+                physical_device,
+                buffer_size,
+                c.VK_IMAGE_USAGE_TRANSFER_DST_BIT | buffer_type,
+                c.VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+            );
+            errdefer data_buffer.deinit();
 
-        const allocate_info = c.VkCommandBufferAllocateInfo{
-            .sType = c.VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
-            .level = c.VK_COMMAND_BUFFER_LEVEL_PRIMARY,
-            .commandPool = command_pool,
-            .commandBufferCount = 1,
-        };
+            const allocate_info = c.VkCommandBufferAllocateInfo{
+                .sType = c.VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+                .level = c.VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+                .commandPool = command_pool,
+                .commandBufferCount = 1,
+            };
 
-        var command_buffer: c.VkCommandBuffer = undefined;
-        try wrapVulkanResult(
-            c.vkAllocateCommandBuffers(device, &allocate_info, &command_buffer),
-            "failed to allocate copying command buffer",
-        );
-        defer c.vkFreeCommandBuffers(device, command_pool, 1, &command_buffer);
+            var command_buffer: c.VkCommandBuffer = undefined;
+            try wrapVulkanResult(
+                c.vkAllocateCommandBuffers(device, &allocate_info, &command_buffer),
+                "failed to allocate copying command buffer",
+            );
+            defer c.vkFreeCommandBuffers(device, command_pool, 1, &command_buffer);
 
-        const begin_info = c.VkCommandBufferBeginInfo{
-            .sType = c.VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
-            .flags = c.VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
-        };
-        try wrapVulkanResult(
-            c.vkBeginCommandBuffer(command_buffer, &begin_info),
-            "failed to begin recording copying command buffer",
-        );
+            const begin_info = c.VkCommandBufferBeginInfo{
+                .sType = c.VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+                .flags = c.VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
+            };
+            try wrapVulkanResult(
+                c.vkBeginCommandBuffer(command_buffer, &begin_info),
+                "failed to begin recording copying command buffer",
+            );
 
-        const copy_region = c.VkBufferCopy{
-            .size = buffer_size,
-        };
-        c.vkCmdCopyBuffer(
-            command_buffer,
-            staging_buffer.handle,
-            vertex_buffer.handle,
-            1,
-            &copy_region,
-        );
+            const copy_region = c.VkBufferCopy{
+                .size = buffer_size,
+            };
+            c.vkCmdCopyBuffer(
+                command_buffer,
+                staging_buffer.handle,
+                data_buffer.handle,
+                1,
+                &copy_region,
+            );
 
-        try wrapVulkanResult(
-            c.vkEndCommandBuffer(command_buffer),
-            "failed to end recording copying command buffer",
-        );
+            try wrapVulkanResult(
+                c.vkEndCommandBuffer(command_buffer),
+                "failed to end recording copying command buffer",
+            );
 
-        const submit_info = c.VkSubmitInfo{
-            .sType = c.VK_STRUCTURE_TYPE_SUBMIT_INFO,
-            .commandBufferCount = 1,
-            .pCommandBuffers = &command_buffer,
-        };
-        try wrapVulkanResult(
-            c.vkQueueSubmit(graphics_queue, 1, &submit_info, null),
-            "failed to submit copying command to queue",
-        );
-        try wrapVulkanResult(
-            c.vkQueueWaitIdle(graphics_queue),
-            "failed to wait for queue to complete",
-        );
+            const submit_info = c.VkSubmitInfo{
+                .sType = c.VK_STRUCTURE_TYPE_SUBMIT_INFO,
+                .commandBufferCount = 1,
+                .pCommandBuffers = &command_buffer,
+            };
+            try wrapVulkanResult(
+                c.vkQueueSubmit(graphics_queue, 1, &submit_info, null),
+                "failed to submit copying command to queue",
+            );
+            try wrapVulkanResult(
+                c.vkQueueWaitIdle(graphics_queue),
+                "failed to wait for queue to complete",
+            );
 
-        return VertexBuffer{
-            .vertices = vertices,
-            .handle = vertex_buffer,
-        };
-    }
+            return SelfBuffer{
+                .data = data,
+                .handle = data_buffer,
+            };
+        }
 
-    pub fn deinit(self: VertexBuffer) void {
-        self.handle.deinit();
-    }
-};
+        pub fn deinit(self: SelfBuffer) void {
+            self.handle.deinit();
+        }
+    };
+}
+
+pub const VertexBuffer = DataBuffer(Vertex, c.VK_BUFFER_USAGE_VERTEX_BUFFER_BIT);
+pub const IndexBuffer = DataBuffer(u16, c.VK_BUFFER_USAGE_INDEX_BUFFER_BIT);
